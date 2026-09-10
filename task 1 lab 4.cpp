abdc8be328f527cca3f06a3b8a23e4f1522bc6ff
hassaanraza0113-prog <hassaan.raza0113@gmail.com>
2026-09-10 17:56:24 +0500

    Add files via upload

---
+++ task 1 lab 4.cpp
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int main()
+{
+    float num1, num2, num3;
+
+    printf("Enter any three numbers: ");
+    scanf("%f %f %f", &num1, &num2, &num3);
+
+// check if numbers are equal
+    if (num1 == num2 && num2 == num3)
+    {
+        printf("All three numbers are equal to %.2f\n", num1);
+    }
+    //check if number 1 is greatest 
+    else if (num1 >= num2 && num1 >= num3)
+    {
+        if (num1 == num2)
+        {
+            printf("The greatest value is %.2f (First and second numbers are equal).\n", num1);
+        }
+        else if (num1 == num3)
+        {
+            printf("The greatest value is %.2f (First and third numbers are equal).\n", num1);
+        }
+        else
+        {
+            printf("The greatest number is %.2f\n", num1);
+        }
+    }
+    // check if number 2 is greatest
+    else if (num2 >= num1 && num2 >= num3)
+    {
+        if (num2 == num3)
+        {
+            printf("The greatest value is  %.2f (Second and third numbers are equal).\n", num2);
+        }
+        else
+        {
+            printf("The greatest number is %.2f\n", num2);
+        }
+    }
+    else
+    {
+        printf("The greatest number is %.2f\n", num3);
+    }
+
+    return 0;
+}
